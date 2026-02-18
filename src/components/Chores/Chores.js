76b566d3f5147e00e8655f4974d7dc08c8612577
bear <bearawrbyne@gmail.com>
2026-02-19 00:17:34 +0800

    init components

---
+++ src/components/Chores/Chores.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Weight/Weight.css';
+
+const Chores = () => {
+    return (
+        <div className='placeholder-view fade-in'>
+            <div className='placeholder-content'>
+                <h1>Chores Tracking</h1>
+                    <p>Feature coming soon.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Chores;
